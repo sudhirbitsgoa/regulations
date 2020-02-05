3846e4b167c186356107bf090a1754e524dac147
sudhir <sudhirbitsgoa@gmail.com>
2020-02-05 08:02:21 +0530

    feat(restrict): restrict user to get his own questions;

--- server/models/response.js
+++ server/models/response.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function(Response) {
+module.exports = function (Response) {
+	async function resolveUser(ctx) {
+		const req = ctx.req;
+		const token = req.headers["access_token"] || req.query.access_token;
+		const AccessToken = Response.app.models.AccessToken;
+		const UserModel = Response.app.models.User;
+		const details = await AccessToken.findOne({
+			where: {
+				id: token
+			}
+		});
+		const user = await UserModel.findOne({
+			where: {
+				id: details.userId.toString()
+			}
+		});
+		return user;
+	}
 
+	Response.beforeRemote('find', async (ctx, output) => {
+		const user = await resolveUser(ctx);
+		if (!ctx.result) {
+			return;
+		}
+		if (ctx.args && ctx.args.filter) {
+			const where = ctx.args.filter.where;
+			where['userId'] = user.id;
+		} else {
+			ctx.args.filter = {
+				where : {
+					userId: user.id
+				}
+			};
+		}
+	});
 };
